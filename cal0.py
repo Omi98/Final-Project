2d4d36a84ec53b74173e8855585378ae030b28ae
Omama K <omamak1998@gmail.com>
2024-03-11 23:15:11 +0300

    functions

---
+++ cal0.py
@@ -0,0 +1,12 @@
+# sum of two numbers
+
+
+# addition
+def sum(num1, num2):
+    return num1 + num2
+
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print(sum(num1, num2))
---
+++ cal1.py
@@ -0,0 +1,44 @@
+# sum and difference of two numbers
+# choice of user
+
+
+# addition
+def add(num1, num2):
+    return f"The sum of {num1} and {num2} is {num1 + num2}"
+
+
+# subtraction
+def subtract(num1, num2):
+    return f"The difference of {num1} and {num2} is {num1 - num2}"
+
+
+# product
+def multiply(num1, num2):
+    return f"The product of {num1} and {num2} is {num1 * num2}"
+
+
+# quotient
+def divide(num1, num2):
+    return f"The quotient of {num1} and {num2} is {num1 // num2}"
+
+
+# operation
+def oper(choice, num1, num2):
+    match choice:
+        case 1:
+            print(add(num1, num2))
+        case 2:
+            print(subtract(num1, num2))
+        case 3:
+            print(multiply(num1, num2))
+        case 4:
+            print(divide(num1, num2))
+        case _:
+            print("Enter the choice between 1 and 4")
+
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+choice = int(input("Which opetion you want to perform?\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\nChoice: "))
+answer = oper(choice, num1, num2)
